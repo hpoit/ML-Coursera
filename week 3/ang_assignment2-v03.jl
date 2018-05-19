895c8e9ecee4cc11321eafc6afb9789fd0924a45
Hpoit <kvtliu@gmail.com>
2018-05-19 20:16:19 -0300

    optimized J(θ) as expected

--- week 3/ang_assignment2-v03.jl
+++ week 3/ang_assignment2-v03.jl
@@ -1,5 +1,6 @@
 # Logit mode, v0.3
 
+# Part 2: compute cost and gradient
 using CSV, Plots; pyplot();
 data = CSV.read("/Users/kevinliu/Documents/machine-learning-ex2/ex2/ex2data1.txt", datarow=1)
 
@@ -30,8 +31,8 @@ h([-24, 0.2, 0.2], X)
 function cost(θ, X, y)
     hx = sigmoid(X * θ)
     m = length(y)
-    J = (-y' * log.(hx) - (1 - y') * log.(1 - hx)) / m
-    grad = X' * (hx - y) / m
+    global J = (-y' * log.(hx) - (1 - y') * log.(1 - hx)) / m
+    global grad = X' * (hx - y) / m
     println("Cost is $J")
     println("Gradient is $grad")
 end
@@ -44,4 +45,30 @@ cost([0, 0, 0], X, y)
 cost([-24, 0.2, 0.2], X, y)
 # Cost is 0.21833019382659777
 # Gradient is [0.042903, 2.56623, 2.6468]
+# as expected
+
+# Part 3: optimize J and gradient
+using Optim
+
+# simple case: minimize x²
+f(x) = x[1]^2
+optimize(f, [0.0,0.0])
+
+# simple case: minimize a * x²
+f(x) = 100 * x[1]^2
+optimize(f, [0.0,0.0])
+
+# simple case: minimize x² + y²
+f(x) = x[1]^2 + y[2]^2
+optimize(f, zeros(2))
+
+# minimize J and gradient
+J(θ) = (-y' * log.(sigmoid(X * θ)) - (1 - y') * log.(1 - sigmoid(X * θ))) / length(y)
+optimize(J, zeros(3), ConjugateGradient())
+
+Optim.minimizer(optimize(J, zeros(3), ConjugateGradient()))
+# => 3-element Array{Float64,1}:
+#     -25.1614
+#       0.206232
+#       0.201472
 # as expected
