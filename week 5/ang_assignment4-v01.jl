1ecb410434e7a1a857424418fc05510b3f508e46
Hpoit <kvtliu@gmail.com>
2018-06-11 10:55:14 -0300

    lay out Ng's 10-part process v. Flux

---
+++ week 5/ang_assignment4-v01.jl
@@ -0,0 +1,13 @@
+# A. Ng's process
+# Part 1: load and visualize data
+# Part 2: load parameters
+# Part 3: compute cost (feedforward)
+# Part 4: regularization
+# Part 5: sigmoid gradient
+# Part 6: initialize parameters
+# Part 7: backpropagation
+# Part 8: train NN
+# Part 9: visualize trained parameters (weights)
+# Part 10: predict
+
+# Julia Flux process 
