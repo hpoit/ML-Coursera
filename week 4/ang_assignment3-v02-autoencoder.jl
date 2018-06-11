ea87511b40da6943d85a3f0a1b2cc293a42c796a
Hpoit <kvtliu@gmail.com>
2018-06-11 09:29:12 -0300

    add autoencoder Wikipedia definition

--- week 4/ang_assignment3-v02-autoencoder.jl
+++ week 4/ang_assignment3-v02-autoencoder.jl
@@ -1,5 +1,12 @@
 # Autoencoder (thanks Mike Innes)
 
+"""
+An autoencoder is a type of artificial neural network used to learn efficient
+data codings in an unsupervised manner. The aim of an autoencoder is to learn
+a representation (encoding) for a set of data, typically for the purpose of
+dimensionality reduction.
+"""
+
 using Flux, Flux.Data.MNIST
 using Flux: @epochs, onehotbatch, argmax, mse, throttle
 using Base.Iterators: partition
